982235ed34905cf280f1224618e0a4859a755bfb
Carolina Mack <cmack10@uw.edu>
2023-01-18 19:07:48 -0800

    made script executable

--- download_pronto.sh
+++ download_pronto.sh
@@ -6,7 +6,4 @@ cd pronto_data
 
 curl https://data.seattle.gov/api/views/tw7j-dfaw/rows.csv?accessType=DOWNLOAD -o pronto.csv
 head pronto.csv
-<<<<<<< HEAD
-=======
 
->>>>>>> 5ede5570e4ee9f2f6b7728e9974cac0ea0585627
